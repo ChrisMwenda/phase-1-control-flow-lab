7083a9fa5bc135551f31f7dc72dfffa54b515ef9
Christopher <chrismbogo97@gmail.com>
2022-08-20 13:51:12 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet){
+  let result;
+  if(distanceInFeet<=400){
+   result="This one is on me!"
+   return result;
+}
+else if(distanceInFeet >=2000 && distanceInFeet<2500){
+  result="I will gladly take your thirty bucks."
+  return result;
+} 
+else result="No can do."
+return result;
+}
+console.log(scuberGreetingForFeet(3000));
+
+
+function ternaryCheckCity(city){
+let result;
+result=city=="NYC"?"Ok, sounds good.":"No go.";
+return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+ternaryCheckCity("NYC");
+
+function switchOnCharmFromTip (typeOfTip){
+  switch (typeOfTip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+switchOnCharmFromTip("generous");
